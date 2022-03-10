abd38815328c06513b8598e791cfcedae37be49f
thejackle <jpavan@hotmail.ca>
2022-03-09 22:13:09 -0500

    Adding comments

---
+++ src/serialCommands.h
@@ -0,0 +1,23 @@
+// Request and acknowledge
+#define SER_REQ 0x00
+#define SER_ACK 0x01
+
+// Get and set commands
+#define SER_GETTEMP 0x05
+#define SER_SETTEMP 0x06
+
+#define SER_GETFAN 0x07
+#define SER_SETFAN 0x08
+
+#define SER_GETCOOL 0x09
+#define SER_SETCOOL 0x0A
+
+// Commands for setting
+#define SER_FAN0 0x10
+#define SER_FAN1 0x11
+#define SER_FAN2 0x12
+#define SER_FAN3 0x13
+
+#define SER_COOL0 0x20
+#define SER_COOL1 0x21
+#define SER_COOL2 0x22

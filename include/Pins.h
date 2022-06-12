0d20e74b488c1e1a15fb1a073ce3917698ec2db1
thejackle <jpavan@hotmail.ca>
2022-06-11 22:26:58 -0400

    Updated power control pins

--- include/Pins.h
+++ include/Pins.h
@@ -4,10 +4,10 @@
 #define TEMPSENSOR_PIN 34
 
 // Power relay output
-#define FAN_LOW_PIN 25
-#define FAN_MEDIUM_PIN 26
-#define FAN_HIGH_PIN 27
-#define COOLER_PIN 28
+#define FAN_LOW_PIN 28
+#define FAN_MEDIUM_PIN 27
+#define FAN_HIGH_PIN 26
+#define COOLER_PIN 25
 
 #define TEST_TEMPIN 23
 

3b03bcdc4f0c953204f59843b3535556a250844e
thejackle <jpavan@hotmail.ca>
2023-06-30 14:30:10 -0400

    add defines for OLED display 1x4 keypad MCP9808 IR remote

--- include/Settings.h
+++ include/Settings.h
@@ -1,5 +1,5 @@
 // Define for bench testing
-//#define TESTBED
+// #define TESTBED
 
 // Program version
 #define SW_Version "Smart Air  V3.0"
@@ -10,10 +10,22 @@
     #define NO_UPDATE 0
 
 // Keypad setup
+    // #define FOURxFOUR
+    #define ONExFOUR
+
 // Heartbeat LED setup
 
+
 // LCD setup
-    #define LCD_UPDATE_DELAY 1
+    // #define LCD_DISPLAY
+    #define OLED_DISPLAY
+
+    #ifdef LCD_DISPLAY
+        #include <LiquidCrystal_I2C.h>
+        #define LCD_UPDATE_DELAY 1
+    #elif defined(OLED_DISPLAY)
+        #include <Adafruit_SSD1306.h>
+    #endif
 
 // Backlight
     // Timeout setting
@@ -28,8 +40,16 @@
     #define TEMP_DELAY 1000
     #define MCP9808
     // #define SENS_DHT11
-    //#define DS18B20
-    //#define CONST_TEMP
+    // #define DS18B20
+    // #define CONST_TEMP
+
+    #ifdef MCP9808
+        #include <Adafruit_MCP9808.h>
+    #elif defined(SENS_DHT11)
+        #include <DHT.h>
+    #elif defined(DS18B20)
+        #include <DallasTemperature.h>
+    #endif
 
 // Temp controller
     #ifdef TESTBED
@@ -47,12 +67,16 @@
 // Net update
     #define NET_UPDATE_DELAY 3000
 
+// IR Remote
+    #define IR_REMOTE
+
 // Power controls
     #define FAN_LOW 1
     #define FAN_MEDIUM 2
     #define FAN_HIGH 3
 
 // Segment display
+    // #define SEGMENT_DISPLAY
     #define SEGMENT_DELAY 1000
 
 // Menu Setup

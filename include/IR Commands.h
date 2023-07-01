55290c8935b156e4f9f053242780078519f4d217
thejackle <jpavan@hotmail.ca>
2023-07-01 13:20:18 -0400

    Add IR command defines

---
+++ include/IR Commands.h
@@ -0,0 +1,15 @@
+
+#define IR_RECEIVER_PIN 32
+#define IR_SEND_PIN_a 9
+
+#define BTN_SELECT 57
+#define BTN_UP 58
+#define BTN_DOWN 59
+#define BTN_LEFT 62
+#define BTN_RIGHT 63
+
+#define BTN_ZOOM_UP 74
+#define BTN_ZOOM_DOWN 75
+
+#define BTN_START_STOP 72
+#define BTN_2_SEC 55
